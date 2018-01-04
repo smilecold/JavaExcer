0a014e9cd9ffc2e43905123f237e61943efdc4e6
smilecold <smilecold@users.noreply.github.com>
2018-01-04 10:25:17 +0800

    例题导入

---
+++ test/src/test/test.java
@@ -0,0 +1,14 @@
+package test;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a=new String("ajdlfjaldhfalkhdfalhiuenv");
+		for (int i = 0; i < a.length(); i++) {
+			System.out.println(a.substring(i));
+		}
+		
+	}
+
+}
