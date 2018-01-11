b407b3f1762f9975e4c4aa5bd0ca600c938bb769
smilecold <smilecold@users.noreply.github.com>
2018-01-11 21:03:26 +0800

    thread

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -1,14 +1,39 @@
+
 package test;
 
 public class test {
-
+	public static int i = 0;
+	public static int j = 0;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String a=new String("ajdlfjaldhfalkhdfalhiuenv");
-		for (int i = 0; i < a.length(); i++) {
-			System.out.println(a.substring(i));
+		Run1 r1 = new Run1();
+		Run2 r2 = new Run2();
+		Thread t1 = new Thread(r1);
+		Thread t2 = new Thread(r2);
+		t2.setPriority(10);
+		t1.start();
+		t2.start();
+	}
+	
+}
+class Run1 implements Runnable{
+
+	@Override
+	public void run() {
+		for (; test.i < 10; test.i++) {
+			System.out.println(test.i);
 		}
 		
 	}
-
+	
+}
+class Run2 implements Runnable{
+	@Override
+	public void run() {
+		while (test.i<10) {
+			if(test.i>test.j) {
+				System.out.println("------"+test.i);
+				test.j=test.i;
+			}
+		}
+	}
 }
